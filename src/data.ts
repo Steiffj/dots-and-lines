a17f91a89516d4eacffab94a2337320da23df5a0
Steiffj <18404081+Steiffj@users.noreply.github.com>
2025-10-30 23:19:33 -0400

    Set up graph styling based on Sigma's performance showcase, but with different color generation method.

--- src/data.ts
+++ src/data.ts
@@ -2,12 +2,17 @@ import Graph from "graphology";
 import clusters from "graphology-generators/random/clusters";
 import circlepack from "graphology-layout/circlepack";
 import type { EdgeDisplayData, NodeDisplayData } from "sigma/types";
+import type { ColorPalette } from "./palettes";
+import chroma from "chroma-js";
 
-export type NodeAttrs = Partial<NodeDisplayData>;
+export type NodeAttrs = Partial<NodeDisplayData> & {
+  cluster?: number;
+};
 export type EdgeAttrs = Partial<EdgeDisplayData>;
 export type GraphAttrs = {
-  theme: "dark" | "light";
-  text: string;
+  theme?: "dark" | "light";
+  text?: string;
+  palette?: ColorPalette;
 };
 
 export type VisGraph = Graph<NodeAttrs, EdgeAttrs, GraphAttrs>;
@@ -20,8 +25,28 @@ export function graphDirectedLoops(): VisGraph {
   });
 }
 
-export function createTestGraph() {
-  const g = clusters(Graph, { order: 3500, size: 6000, clusters: 9 });
+export function createTestGraph(
+  options: { order: number; size: number; clusters: number },
+  palette: ColorPalette
+) {
+  const g = clusters(Graph, options) as VisGraph;
   circlepack.assign(g, { hierarchyAttributes: ["cluster"] });
+  const colors = chroma
+    .scale(palette.span)
+    .mode("lch")
+    .colors(options.clusters);
+
+  let i = 0;
+  g.updateEachNodeAttributes((node, attrs) => {
+    const cluster = attrs.cluster;
+    return {
+      ...attrs,
+      size: g.degree(node) / 2,
+      label: `Node n°${++i}`,
+      color: colors[cluster!],
+    };
+  });
+
+  g.setAttribute("palette", palette);
   return g;
 }
---
+++ src/palettes.ts
@@ -0,0 +1,17 @@
+export type ColorPalette = {
+  name: string;
+  span: [string, string];
+  edge: string;
+};
+
+export const PALETTE_HIGH_CONTRAST: ColorPalette = {
+  name: "https://coolors.co/palette/a2d1dd-ff91af-dfff00-ff8000-320e3b",
+  span: ["#dfff00", "#320e3b"],
+  edge: "#6b6f72",
+};
+
+export const PALETTE_HOT: ColorPalette = {
+  name: "https://coolors.co/palette/f42b03-f53f0f-f5521b-f66627-f77a33-f78d3f-f8a14b-f9b557-f9c863-fadc6f",
+  span: ["#f42b03", "#fadc6f"],
+  edge: "#403f4c",
+};
