8e40ea681a084583ef8b72f4efa16f42cd0f0543
Steiffj <18404081+Steiffj@users.noreply.github.com>
2025-11-02 15:07:29 -0500

    Start organizing visual theme/color schemes.

---
+++ src/color-scheme/palettes.ts
@@ -0,0 +1,13 @@
+import type { ColorPalette } from "./types";
+
+export const PALETTE_HIGH_CONTRAST: ColorPalette = {
+  name: "High Contrast",
+  span: ["#dfff00", "#320e3b"],
+  edge: "#6b6f72",
+};
+
+export const PALETTE_HOT: ColorPalette = {
+  name: "Hot",
+  span: ["#f42b03", "#fadc6f"],
+  edge: "#403f4c",
+};
