5c8081169b9ada45b5c184ae2da5f63d91f88591
Steiffj <18404081+Steiffj@users.noreply.github.com>
2025-11-02 23:18:24 -0500

    Drag and drop legibility improvements and minor perf improvement (still not actually optimized yet).

--- src/features/drag-and-drop.ts
+++ src/features/drag-and-drop.ts
@@ -1,5 +1,10 @@
-import type { DALSigma } from "../dal-types";
+import type { DALSettings, DALSigma } from "../dal-types";
 import type { FeatureRegistration } from "./types";
+
+/**
+ * @todo will want a utility to manage this for similar situations
+ */
+const originalSettings: Partial<DALSettings> = {};
 
 const featDragAndDrop: FeatureRegistration = (events, reducers) => {
   events.register("enterNode", (sigma) => {
@@ -26,29 +31,28 @@ const featDragAndDrop: FeatureRegistration = (events, reducers) => {
     }
   });
 
-  // TODO not working as-is; check what Sigma is doing with its default renderers/event handling
-
-  // events.register("enterNode", (sigma, payload) => {
-  //   const state = sigma.getGraph().getAttribute("uiState");
-  //   if (state.dragging) {
-  //     payload.preventSigmaDefault();
-  //     payload.event.original.preventDefault();
-  //     payload.event.original.stopPropagation();
-  //   }
-  // });
-
   // Update dragging position
   events.register("moveBody", (sigma, payload) => {
     const g = sigma.getGraph();
     const state = g.getAttribute("uiState");
-    const node = state.dragging;
-    if (!node) {
+    const draggedNode = state.dragging;
+    if (!draggedNode) {
       return;
     }
 
+    // if (sigma.getSetting("renderEdgeLabels")) {
+    //   originalSettings.renderEdgeLabels = true;
+    //   sigma.setSetting("renderEdgeLabels", false);
+    // }
+
+    if (sigma.getSetting("renderLabels")) {
+      originalSettings.renderLabels = true;
+      sigma.setSetting("renderLabels", false);
+    }
+
     const pos = sigma.viewportToGraph(payload.event);
-    g.setNodeAttribute(node, "x", pos.x);
-    g.setNodeAttribute(node, "y", pos.y);
+    g.setNodeAttribute(draggedNode, "x", pos.x);
+    g.setNodeAttribute(draggedNode, "y", pos.y);
 
     payload.event.preventSigmaDefault();
     payload.event.original.preventDefault();
@@ -60,15 +64,35 @@ const featDragAndDrop: FeatureRegistration = (events, reducers) => {
     sigma.getContainer().style.cursor = "initial";
     const state = sigma.getGraph().getAttribute("uiState");
     const draggedNode = state.dragging;
-    if (draggedNode) {
-      sigma.setCustomBBox(null);
-      state.dragEnd();
-      sigma.scheduleRefresh({
-        partialGraph: {
-          nodes: [draggedNode],
-        },
-      });
+    if (!draggedNode) {
+      return;
     }
+
+    sigma.setCustomBBox(null);
+    state.dragEnd();
+
+    // TODO refactor
+    // if (
+    //   originalSettings.renderEdgeLabels !== undefined &&
+    //   sigma.getSetting("renderEdgeLabels") !== originalSettings.renderEdgeLabels
+    // ) {
+    //   sigma.setSetting("renderEdgeLabels", originalSettings.renderEdgeLabels);
+    //   delete originalSettings.renderEdgeLabels;
+    // }
+
+    if (
+      originalSettings.renderLabels !== undefined &&
+      sigma.getSetting("renderLabels") !== originalSettings.renderLabels
+    ) {
+      sigma.setSetting("renderLabels", originalSettings.renderLabels);
+      delete originalSettings.renderLabels;
+    }
+
+    sigma.scheduleRefresh({
+      partialGraph: {
+        nodes: [draggedNode],
+      },
+    });
   };
 
   events.register("upNode", drop);
@@ -79,12 +103,49 @@ const featDragAndDrop: FeatureRegistration = (events, reducers) => {
     let display = pooled ?? {};
     const g = sigma.getGraph();
     const state = g.getAttribute("uiState");
-    if (state.dragging === node) {
-      display.highlighted = true;
+
+    const draggedNode = state.dragging;
+    if (!draggedNode) {
+      return display;
     }
-    // else if (state.dragging && state.dragging !== node) {
-    //   display.highlighted = false;
-    // }
+
+    if (draggedNode === node) {
+      display.highlighted = true;
+      display.forceLabel = true;
+      return display;
+    }
+
+    const neighbors = g.neighbors(state.dragging);
+    if (neighbors.includes(node)) {
+      display.highlighted = true;
+      display.forceLabel = true;
+    } else {
+      display.label = undefined;
+      display.highlighted = false;
+    }
+
+    return display;
+  });
+
+  reducers.edge.register((edge, data, pooled, sigma) => {
+    let display = pooled ?? {};
+    const g = sigma.getGraph();
+    const state = g.getAttribute("uiState");
+    if (!state.dragging) {
+      return display;
+    }
+
+    if (
+      g.source(edge) === state.dragging ||
+      g.target(edge) === state.dragging
+    ) {
+      display.label = display.label ?? data.label;
+      display.forceLabel = true;
+    } else {
+      display.label = undefined;
+      // display.hidden = true;
+    }
+
     return display;
   });
 };
