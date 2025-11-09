eac23ce86bc5142a3e1a452dbb77f1208d575102
Steiffj <18404081+Steiffj@users.noreply.github.com>
2025-11-09 13:59:08 -0500

    Define New feature types/interfaces. Refactor core feature to fit new defs. (Other feature types are broken for the moment.)

--- src/features/core.feat.ts
+++ src/features/core.feat.ts
@@ -4,16 +4,36 @@ import {
   edgeReducerPoolInit,
 } from "../reducers/edge-reducers";
 import { nodeReducerCommon } from "../reducers/node-reducers";
-import type { FeatureDefinition } from "./types";
+import type {
+  EdgeReducerRegistry,
+  NodeReducerRegistry,
+} from "../reducers/reducer.registry";
+import type { Feature, FeatureInit, FeatureState } from "./types";
 
-const FEAT = Symbol("core");
-export const FEAT_CORE = FEAT;
-const featCore: FeatureDefinition = (_, reducers) => {
-  reducers.node.register(FEAT, nodeReducerCommon);
-  reducers.edge.register(FEAT, edgeReducerPoolInit);
-  reducers.edge.register(FEAT, edgePaletteReducer); // TODO maybe move into a color scheme feature when that becomes more sophisticated
-  reducers.edge.register(FEAT, defaultEdgeLabels);
-  return FEAT;
-};
+export class FeatCore implements Feature, FeatureInit {
+  readonly id = Symbol("core");
+  state: FeatureState = { active: true };
+  get active() {
+    return true;
+  }
 
-export default featCore;
+  constructor(
+    private readonly nodeReducers: NodeReducerRegistry,
+    private readonly edgeReducers: EdgeReducerRegistry
+  ) {}
+
+  start() {
+    return this.state;
+  }
+  stop() {
+    return this.state;
+  }
+
+  init(): Feature {
+    this.nodeReducers.register(this.id, nodeReducerCommon);
+    this.edgeReducers.register(this.id, edgeReducerPoolInit);
+    this.edgeReducers.register(this.id, edgePaletteReducer); // TODO maybe move into a color scheme feature when that becomes more sophisticated
+    this.edgeReducers.register(this.id, defaultEdgeLabels); // TODO temporary
+    return this;
+  }
+}
